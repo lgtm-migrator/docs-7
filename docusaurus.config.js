8ff05957dea7fb33eee14a5099445e72cfd0098e
kizahasi <rasis.aneki@gmail.com>
2021-12-21 23:53:50 +0900

    個人鯖→自鯖

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: "Flocon（フロコン）",
-  tagline: "個人サーバーで使えるTRPGオンラインセッションツール（β）",
+  tagline: "自鯖で使えるTRPGオンラインセッションツール",
   url: "https://flocon.app",
   baseUrl: "/",
   onBrokenLinks: "throw",
